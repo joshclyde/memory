025916cfbe37f4594d629124bd949b9a63d12176
Josh Clyde <joshclyde11@gmail.com>
2022-05-01 11:20:15 -0400

    Comment out shims-vue.d.ts
    I had an error in my app when trying to import a component from
    a .vue file that did not exist. However, typescript and my IDE did
    not catch the error because this shim was saying that any .vue
    file is okay even if it doesn't exist? So wanna remove it for now,
    not sure the purpose of it in the first place.

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -4,9 +4,9 @@
 /// <reference types="vue/ref-macros" />
 
 // Mocks all files ending in `.vue` showing them as plain Vue instances
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
-  const component: DefineComponent<{}, {}, any>
-  export default component
-}
+// declare module '*.vue' {
+//   import type { DefineComponent } from 'vue'
+//   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
+//   const component: DefineComponent<{}, {}, any>
+//   export default component
+// }
